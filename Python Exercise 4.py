bc0c7153d4d68b4fbec91d90453f9a4ba7d8daa4
Sudarshen <sudarshenk@gmail.com>
2024-04-29 05:08:40 +0200

    4 update

--- Python Exercise 4.py
+++ Python Exercise 4.py
@@ -1,7 +1,7 @@
+# # Testing Git and Git push through git bash terminal! result: successful!
 # print("yow")
 # age = input("Enter your age: ")
-#
 # print(f"Your age is {age}!")
+# print("something")
 
-print("something")
-
+# # Exercise 1
