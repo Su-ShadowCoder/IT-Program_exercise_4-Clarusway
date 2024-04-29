25876172dd4222ec585839823a610b9b028d8186
Sudarshen <sudarshenk@gmail.com>
2024-04-29 06:48:32 +0200

    @ second Exercise of exercise 4

--- Python Exercise 4.py
+++ Python Exercise 4.py
@@ -4,4 +4,31 @@
 # print(f"Your age is {age}!")
 # print("something")
 
-# # Exercise 1
+#  # Exercise 1
+# list_1 = input("Enter numbers in comma within brackets[]"
+#                "(or enter nothing at all),\n"
+#                "the numbers you have entered "
+#                "will change into a list.\n"
+#                "The program wil then check if the "
+#                "list is empty or not: ")
+# if len(list_1) == 0:
+#     print("This list IS empty!")
+# elif list_1 == "[]":
+#     print("This list IS empty!")
+# else:
+#     print("This list is NOT empty!")
+
+# Exercise 2
+data_lst = []
+
+while True:
+    number = input("Enter a whole number to make a list: ")
+    data_lst.append(number)
+
+    choice = input("Enter another number? (y/n): ")
+    if choice.casefold() == "n":
+        break
+data_lst.sort()
+print(data_lst)
+
+
